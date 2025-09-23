93d5372e2468a2922903aa2ebf8a345188c3443c
Danilo Chaves <danchavesds@gmail.com>
2025-09-23 14:57:03 -0300

    feat: Alteração do EscalaRequestDTO para escala personalizada

--- src/main/java/br/gov/agu/nutec/msescala/dto/request/EscalaRequestDTO.java
+++ src/main/java/br/gov/agu/nutec/msescala/dto/request/EscalaRequestDTO.java
@@ -3,12 +3,16 @@ package br.gov.agu.nutec.msescala.dto.request;
 import br.gov.agu.nutec.msescala.enums.Uf;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public record EscalaRequestDTO(
         Integer setorOrigemId,
         Integer especieTarefaId,
         LocalDate dataInicio,
         LocalDate dataFim,
-        Uf uf
+        Uf uf,
+        List<Long> orgaoJulgadorIds,
+        List<Long> avaliadorIds,
+        List<Long> pautistaIds
 ) {
 }
