09742062c2c36659aa9d69455ed28d3b1a40f947
DaniloChaves <danchavesds@gmail.com>
2025-10-06 10:54:13 -0300

    Update AvaliadorController to return 201 Created with Location header on successful creation

--- src/main/java/br/gov/agu/nutec/msescala/controller/AvaliadorController.java
+++ src/main/java/br/gov/agu/nutec/msescala/controller/AvaliadorController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+
 @RestController
 @RequestMapping("/avaliador")
 public class AvaliadorController {
@@ -23,7 +25,7 @@ public class AvaliadorController {
     @PostMapping
     public ResponseEntity<AvaliadorResponseDTO> cadastrarAvaliador(@RequestBody @Valid AvaliadorRequestDTO request, @RequestHeader("Authorization") String token) {
         var response = avaliadorService.cadastrarAvaliador(request, token);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.created(URI.create("/avaliador/" + response.avaliadorId())).body(response);
     }
 
     @GetMapping
